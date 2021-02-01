2735ffc9e87d8ee5497b9a92ca4d5276e0cad4ce
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-01 21:38:59 +0600

    added some code in varVs let vs const file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,16 +8,40 @@ import '../styles/index.scss';
 // asdfj;laskd`;
 // console.log(name);
 
-var myName = 'Joney Talukdar';
-var age = 21;
+// var myName = 'Joney Talukdar';
+// var age = 21;
 
-console.log('my name is ' + myName + 'and i am ' + age);
-console.log(
-  `My name is ${myName} and i am ${age} years old .i am ${
-    age < 18 ? 'adult' : ' not adult'
-  }`
-);
+// console.log('my name is ' + myName + 'and i am ' + age);
+// console.log(
+//   `My name is ${myName} and i am ${age} years old .i am ${
+//     age < 18 ? 'adult' : ' not adult'
+//   }`
+// );
 
-console.log(myName.padStart(23));
-console.log(myName.padEnd(20, 'a'));
-console.log('s'.repeat(12));
+// console.log(myName.padStart(23));
+// console.log(myName.padEnd(20, 'a'));
+// console.log('s'.repeat(12));
+
+// var vs let vs const
+
+// if (true) {
+//   let a = 12;
+// }
+
+// for (let i = 0; i < 10; i++) {}
+
+// console.log(i);
+
+// console.log(a);
+
+{
+  let i = 'i am let';
+  console.log(i);
+}
+
+// usign iife
+
+(function () {
+  var abc = 'ABC';
+  console.log(abc);
+})();
